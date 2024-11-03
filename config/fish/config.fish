2bda6631a6db043fe6957160e49857bc6d367dce
Alistair Keiller <alistair@keiller.net>
2024-11-03 14:52:59 -0800

    add alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,4 +4,10 @@ end
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
+    alias ls='lsd'
+    alias l='ls -l'
+    alias la='ls -a'
+    alias lla='ls -la'
+    alias lt='ls --tree'
+    alias cd='z'
 end
