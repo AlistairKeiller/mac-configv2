8a760065c338986e1a5d4d4de3a62f671e2db73c
Alistair Keiller <alistair@keiller.net>
2024-12-04 11:09:09 -0800

    add neovim

--- defualt.sh
+++ defualt.sh
@@ -24,7 +24,8 @@ ln -s /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
 brew install --cask --no-quarantine alacritty
 
 # Install misc packages
-brew install mode deno starship uv lsd zoxide mypy
+brew install node deno starship uv lsd zoxide mypy neovim
+git clone https://github.com/NvChad/starter ~/.config/nvim
 
 # Install misc casks 
 brew install --cask visual-studio-code orcaslicer nikitabobko/tap/aerospace modrinth font-jetbrains-mono-nerd-font raspberry-pi-imager cirruslabs/cli/tart \
