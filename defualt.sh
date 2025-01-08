603af5e4c8a2ad046b9e46bb3e10e005ba735d9b
Alistair Keiller <alistair@keiller.net>
2025-01-07 18:08:22 -0800

    remove deno

--- defualt.sh
+++ defualt.sh
@@ -24,7 +24,7 @@ ln -s /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
 brew install --cask --no-quarantine alacritty
 
 # Install misc packages
-brew install node deno starship uv lsd zoxide mypy
+brew install node starship uv lsd zoxide mypy
 
 # Install misc casks 
 brew install --cask visual-studio-code orcaslicer nikitabobko/tap/aerospace modrinth font-jetbrains-mono-nerd-font raspberry-pi-imager \
