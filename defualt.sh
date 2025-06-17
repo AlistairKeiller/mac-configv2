e7176dc5541aacb843d6b2477c6ce77cd8c4222d
Alistair Keiller <alistair@keiller.net>
2025-06-16 17:19:43 -0700

    switch to gh

--- defualt.sh
+++ defualt.sh
@@ -30,27 +30,12 @@ mkdir -p ~/.config/
 cp -r ./config/* ~/.config/
 touch ~/.hushlogin
 
-# Configure git+ssh
+# Configure git
 git config --global user.name "Alistair Keiller"
 git config --global user.email alistair@keiller.net
-mkdir -p ~/.ssh
-cd ~/.ssh
-ssh-keygen -t ed25519 -C "alistair@keiller.net"
-eval "$(ssh-agent -s)"
-ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-touch ~/.ssh/config
-echo "ServerAliveInterval 60" >> ~/.ssh/config
-echo "Host *" >> ~/.ssh/config
-echo "  AddKeysToAgent yes" >> ~/.ssh/config
-echo "  UseKeychain yes" >> ~/.ssh/config
-echo "  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
-git config --global gpg.format ssh
-git config --global user.signingkey ~/.ssh/id_ed25519
-git config --global commit.gpgsign true
-git config --global tag.gpgsign true
-touch ~/.ssh/allowed_signers
-echo "$(git config --get user.email) namespaces=\"git\" $(cat ~/.ssh/id_ed25519.pub)" >> ~/.ssh/allowed_signers
-# copy the key, `pbcopy < ~/.ssh/id_ed25519.pub`, to https://github.com/settings/keys as both an Authentication key and Signing key
+
+# Configure git authentication
+gh auth login
 
 # ctrl+cmd to drag window
 defaults write -g NSWindowShouldDragOnGesture -bool "true"
