8fbe81212520fe6780c658c793cc4ba19a6a262b
Alistair Keiller <alistair@keiller.net>
2026-05-26 07:39:16 -0700

    theme

--- config/sketchybar/colors.lua
+++ config/sketchybar/colors.lua
@@ -1,25 +1,25 @@
 return {
-  black = 0xff181819,
-  white = 0xffe2e2e3,
-  red = 0xfffc5d7c,
-  green = 0xff9ed072,
-  blue = 0xff76cce0,
-  yellow = 0xffe7c664,
-  orange = 0xfff39660,
-  magenta = 0xffb39df3,
-  grey = 0xff7f8490,
+  black = 0xff11111b,   -- crust
+  white = 0xffcdd6f4,   -- text
+  red = 0xfff38ba8,     -- red
+  green = 0xffa6e3a1,   -- green
+  blue = 0xff89b4fa,    -- blue
+  yellow = 0xfff9e2af,  -- yellow
+  orange = 0xfffab387,  -- peach
+  magenta = 0xffcba6f7, -- mauve
+  grey = 0xff7f849c,    -- overlay1
   transparent = 0x00000000,
 
   bar = {
-    bg = 0xf02c2e34,
-    border = 0xff2c2e34,
+    bg = 0xee181825,    -- mantle, slightly transparent
+    border = 0xff181825,
   },
   popup = {
-    bg = 0xc02c2e34,
-    border = 0xff7f8490
+    bg = 0xcc313244,    -- surface0
+    border = 0xff6c7086 -- overlay0
   },
-  bg1 = 0xff363944,
-  bg2 = 0xff414550,
+  bg1 = 0xff313244,     -- surface0
+  bg2 = 0xff45475a,     -- surface1
 
   with_alpha = function(color, alpha)
     if alpha > 1.0 or alpha < 0.0 then return color end
